a0abfa42ad907a5f11c095da2fd31b97ef8271f5
Sapronov Konstantin <ks.costas@gmail.com>
2023-12-10 01:18:12 +0300

    feat: add spinner

--- src/features/todos/routes/Todos.tsx
+++ src/features/todos/routes/Todos.tsx
@@ -2,10 +2,12 @@ import { FC } from 'react';
 
 import { TodosList } from '../components/TodosList';
 import { TodoForm } from '../components/TodoForm';
+import { Spinner } from '@/components/Spinner';
 
 export const Todos: FC = () => {
   return (
     <>
+      <Spinner />
       <TodoForm />
       <TodosList />
     </>
