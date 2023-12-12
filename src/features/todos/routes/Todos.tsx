1ee31399395a320df547e12c63019c1101ee5e10
Sapronov Konstantin <ks.costas@gmail.com>
2023-12-12 13:01:23 +0300

    fix: remove spinner

--- src/features/todos/routes/Todos.tsx
+++ src/features/todos/routes/Todos.tsx
@@ -2,12 +2,10 @@ import { JSX } from 'react';
 
 import { TodosList } from '../components/TodosList';
 import { TodoForm } from '../components/TodoForm';
-import { Spinner } from '@/components/Spinner';
 
 export const Todos = (): JSX.Element => {
   return (
     <>
-      <Spinner />
       <TodoForm />
       <TodosList />
     </>
