8bd43099498a3b6814e538099b0cb4fd5ff5cba2
Sapronov Konstantin <ks.costas@gmail.com>
2023-12-17 12:01:45 +0300

    fix: change todo model

--- src/features/todos/components/TodosList/TodosList.tsx
+++ src/features/todos/components/TodosList/TodosList.tsx
@@ -7,12 +7,12 @@ import { TodosListItem } from '../TodosListItem';
 const todos = [
   {
     id: 'c1a69b93-d3cb-49da-b736-87accbc7852f',
-    title: 'Understand What React Is',
+    text: 'Understand What React Is',
     completed: true,
   },
   {
     id: 'b69ab839-0999-4bc6-a99c-ccc011be6ef3',
-    title: 'Learn How to Set Up your Development Environment',
+    text: 'Learn How to Set Up your Development Environment',
     completed: false,
   },
 ];
