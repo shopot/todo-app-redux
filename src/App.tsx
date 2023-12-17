bd1bab0abbfa321526626f57eeb83bb8b5f6b20a
Sapronov Konstantin <ks.costas@gmail.com>
2023-12-17 23:09:39 +0300

    feat: add provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import { JSX } from 'react';
 
 import styles from './App.module.css';
 
+import { AppProvider } from '@/providers/AppProvider';
 import { Todos } from '@/features/todos';
 
 const App = (): JSX.Element => {
   return (
-    <div className={styles.appContainer}>
-      <h1 className={styles.header}>Todo App</h1>
-      <Todos />
-    </div>
+    <AppProvider>
+      <div className={styles.appContainer}>
+        <h1 className={styles.header}>Todo App</h1>
+        <Todos />
+      </div>
+    </AppProvider>
   );
 };
 
